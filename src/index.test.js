82dff25660b72d5a2de60d81a2a00521c0b7e0f4
Visioness <aygun-08-zurnaci@hotmail.com>
2025-05-28 03:15:13 +0300

    Fix incorrect tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,4 @@
-const {
-  capitalize,
-  reverse,
-  calculator,
-  caesarCipher,
-  analyzeArray
-} = require('./index.js');
+const { capitalize, reverse, calculator, caesarCipher, analyzeArray } = require('./index.js');
 
 /**
  * Capitalizing String
@@ -24,13 +18,13 @@ test('Reverses a string', () => {
  * Calculator Operations
  */
 test('Adds two numbers', () => {
-  expect(calculator.add(1.6, 2.1)).toBe(3.7);
+  expect(calculator.add(1.6, 2.1)).toBeCloseTo(3.7);
   expect(calculator.add(-1, 1)).toBe(0);
   expect(calculator.add(-10, -10)).toBe(-20);
 });
 
 test('Subtracts two numbers', () => {
-  expect(calculator.subtract(3.3, 2.1)).toBe(1.2);
+  expect(calculator.subtract(3.3, 2.1)).toBeCloseTo(1.2);
   expect(calculator.subtract(3, -2)).toBe(5);
   expect(calculator.subtract(4, 12)).toBe(-8);
 });
@@ -38,12 +32,12 @@ test('Subtracts two numbers', () => {
 test('Multiplies two numbers', () => {
   expect(calculator.multiply(1, -3)).toBe(-3);
   expect(calculator.multiply(0, 125)).toBe(0);
-  expect(calculator.multiply(-2, -0.5)).toBe(1);
+  expect(calculator.multiply(-2, -0.5)).toBeCloseTo(1);
   expect(calculator.multiply(2, Infinity)).toBe(Infinity);
 });
 
 test('Divides two numbers', () => {
-  expect(calculator.divide(36, 360)).toBe(0.1);
+  expect(calculator.divide(36, 360)).toBeCloseTo(0.1);
   expect(calculator.divide(1, -1)).toBe(-1);
   expect(calculator.divide(12, Infinity)).toBe(0);
   expect(calculator.divide(Infinity, 120)).toBe(Infinity);
@@ -54,7 +48,7 @@ test('Divides two numbers', () => {
  * Caesar Cipher
  */
 test('Encrypts string with Caesar Algorithm', () => {
-  expect(caesarCipher('CaeSar cipH3r', 3)).toBe('DbfTbs djqI3s');
+  expect(caesarCipher('CaeSar cipH3r', -12)).toBe('QosGof qwdV3f');
   expect(caesarCipher('ThIs texT i5 enCrypted!', 16)).toBe('JxYi junJ y5 udShofjut!');
   expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
 });
@@ -63,7 +57,7 @@ test('Encrypts string with Caesar Algorithm', () => {
  * Array Analysis
  */
 test('Analyzes an array', () => {
-  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toBe({
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual({
     average: 4,
     min: 1,
     max: 8,
